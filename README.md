aa3ec8f07386a0cfd0ecc73b16dd0a34c15e326d
alexrayo <alexrayo.net@gmail.com>
2024-04-05 16:50:51 -0600

    setting up

---
+++ README.md
@@ -0,0 +1,5 @@
+**install postcss-cli
+**** npm i -g postcss-cli
+
+**install pug-cli
+**** npm i -g pug-cli
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, pug, js,}"],
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
